3088313fe2b4cda0dffb30ab92e2b7feec4640df
Tanish Jain <116287000+Tanish-0001@users.noreply.github.com>
2024-08-03 23:55:07 +0530

    Add files via upload

--- chess_board.py
+++ chess_board.py
@@ -4,6 +4,10 @@ import chess.pgn
 
 
 def square_to_index(square):
+    """
+    :param square: integer between 0 and 63
+    :return: coordinates in array indices
+    """
     mapping = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
     position = chess.square_name(square)  # ex: e4
     return 8 - int(position[1]), mapping[position[0]]
@@ -17,50 +21,47 @@ def get_chess_board(board):
             order: pawn, knight, bishop, rook, queen, king
     """
 
-    chess_board = np.zeros((14, 8, 8), dtype=np.int32)
+    chess_board = np.zeros((12, 8, 8), dtype=np.int32)
+    for square, piece in board.piece_map().items():
+        piece_type = piece.piece_type
+        piece_index = (6 + piece_type - 1) if piece.color == chess.BLACK else (piece_type - 1)
+        chess_board[piece_index, 7 - square // 8, square % 8] = -1  # actual piece is represented by a -1
 
-    for piece in chess.PIECE_TYPES:
-        for square in board.pieces(piece, chess.WHITE):
-            i = np.unravel_index(square, (8, 8))
-            chess_board[piece - 1, 7 - i[0], i[1]] = 1
-
-        for square in board.pieces(piece, chess.BLACK):
-            i = np.unravel_index(square, (8, 8))
-            chess_board[piece + 5, 7 - i[0], i[1]] = 1
-
-    if board.turn == chess.WHITE:
-        for move in board.legal_moves:
-            i, j = square_to_index(move.to_square)
-            chess_board[12, i, j] = 1
-
-    else:
-        for move in board.legal_moves:
-            i, j = square_to_index(move.to_square)
-            chess_board[13, i, j] = 1
+    turn = board.turn
+    board.turn = chess.WHITE
+    for move in board.legal_moves:
+        piece = board.piece_at(move.from_square)
+        piece_type = piece.piece_type
+        if piece is not None:
+            piece_index = 6 + piece_type - 1 if piece.color == chess.BLACK else piece_type - 1
+            chess_board[piece_index, 7 - move.to_square // 8, move.to_square % 8] = 1  # legal moves of a piece are represented by a 1
+    board.turn = chess.BLACK
+    for move in board.legal_moves:
+        piece = board.piece_at(move.from_square)
+        piece_type = piece.piece_type
+        if piece is not None:
+            piece_index = 6 + piece_type - 1 if piece.color == chess.BLACK else piece_type - 1
+            chess_board[piece_index, move.to_square // 8, move.to_square % 8] = 1
+    board.turn = turn
 
     return chess_board
 
 
-def square_to_coord(position: int):
+def square_to_uci(position: int):
+    """
+    :param position: integer between 0 and 63
+    :return: string representing a uci square, ex: e4, a6, etc
+    """
     rank = position // 8  # a number
     file = position % 8  # a letter
     mapping = {0: "a", 1: "b", 2: "c", 3: "d", 4: "e", 5: "f", 6: "g", 7: "h"}
     return mapping[file] + str(rank + 1)
 
 
-if __name__ == '__main__':
-    pgn = open("lichess_db_standard_rated_2013-02.pgn")
-    first_game = chess.pgn.read_game(pgn)
+if __name__ == "__main__":
+    # print(square_to_index(0))
+    # print(square_to_uci(0))
+    new_board = chess.Board()
+    new_board.push_uci("e2e4")
+    print(get_chess_board(new_board))
 
-    move_number = 1
-    curr_board = chess.Board()
-
-    for number, curr_move in enumerate(first_game.mainline_moves()):
-        curr_board.push(curr_move)
-
-        # Remember that number starts from 0
-        if number == move_number:
-            break
-
-    print(curr_board)
-    print(get_chess_board(curr_board))
